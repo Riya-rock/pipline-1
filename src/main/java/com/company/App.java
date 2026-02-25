89701e2130d212fe9b5c6e850dd081cbe67eabe7
riyagit <riyamhatre1983@gmail.com>
2026-02-25 10:30:24 +0530

    Update App.java

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello! Maven + Docker Pipeline Running ✅");
+        System.out.println("hello! Maven + Docker Pipeline Running ");
     }
 }
